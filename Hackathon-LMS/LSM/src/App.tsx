0002f8f7e0621e75a72b732a6d5adf1dc194375f
rohittt37 <rohitttgupta808@gmail.com>
2025-03-06 17:00:00 +0530

    Database connected

--- Hackathon-LMS/LSM/src/App.tsx
+++ Hackathon-LMS/LSM/src/App.tsx
@@ -1,7 +1,8 @@
+  
 const App = () => {
   return (
     <div>
-      <h1 className="ttext-3xl font-bold underline">Hello Coders!</h1>      
+      <h1 className="text-3xl font-bold underline">Hello Coders!</h1>      
     </div>
   )
 }
